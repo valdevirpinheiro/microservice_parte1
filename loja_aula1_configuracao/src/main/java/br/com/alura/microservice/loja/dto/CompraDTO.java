bec8294626a9bac444b6f94e07aaa94c0c53e363
valdevir <valdevir@hotmail.com>
2020-05-24 11:19:41 -0300

    first-commit

---
+++ loja_aula1_configuracao/src/main/java/br/com/alura/microservice/loja/dto/CompraDTO.java
@@ -0,0 +1,26 @@
+package br.com.alura.microservice.loja.dto;
+
+import java.util.List;
+
+public class CompraDTO {
+
+	private List<ItemCompraDTO> itens;
+	private EnderecoDTO endereco;
+
+	public List<ItemCompraDTO> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<ItemCompraDTO> itens) {
+		this.itens = itens;
+	}
+
+	public EnderecoDTO getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(EnderecoDTO endereco) {
+		this.endereco = endereco;
+	}
+
+}
